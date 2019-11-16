6ea2378424d88dde15734ea0d8ec741f4ccc0ea7
hodl-farm <hello@hodl.farm>
2019-11-16 00:58:42 -0800

    remove requirement to have the signer online when initializing the node
    Occasionally the signer may be offline, but you should still be able
    to launch and sync the node.
    
    The exception is the first time you launch your setup.

--- docker/tezos-archive-downloader/tezos-archive-downloader.sh
+++ docker/tezos-archive-downloader/tezos-archive-downloader.sh
@@ -6,10 +6,10 @@ bin_dir="/usr/local/bin"
 
 data_dir="/var/run/tezos"
 node_dir="$data_dir/node"
+client_dir="$data_dir/client"
 
 if [ -d ${node_dir}/data/context ]; then
-    echo "Blockchain has already been imported, exiting"
-    exit 0
+    echo "Blockchain has already been imported, skipping"
 else
     echo "Did not find pre-existing data, importing blockchain"
     rm -rvf ${node_dir}/*
@@ -21,4 +21,13 @@ else
     echo "Will download $snapshot"
     curl -L $snapshot | lz4 -d | tar -xvf - -C ${node_dir}/data
     find ${node_dir}
+fi
+
+if [ -z "$PUBLIC_BAKING_KEY" ]; then
+    echo "No public key to import, skipping"
+elif grep $PUBLIC_BAKING_KEY $client_dir/public_key_hashs; then
+    echo "Public key already imported, skipping"
+else
+    echo "Importing public key http://tezos-remote-signer:8445/$PUBLIC_BAKING_KEY"
+    exec "${bin_dir}/tezos-client" --base_dir $client_dir -p $PROTOCOL_SHORT import secret key k8s-baker http://tezos-remote-signer:8445/$PUBLIC_BAKING_KEY -f
 fi
