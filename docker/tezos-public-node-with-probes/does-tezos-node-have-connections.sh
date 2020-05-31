915879e355914454774d5a90a143540cfbc6ddaa
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-05-31 08:34:51 +0800

    revert incomplete change

--- docker/tezos-public-node-with-probes/does-tezos-node-have-connections.sh
+++ docker/tezos-public-node-with-probes/does-tezos-node-have-connections.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # is the number of tezos peer to peer network connections greater than one ?
-[ "$(wget -qO - http://localhost:8732/network/connections | jq '. | length')" -gt $NUM_CONNECTIONS_IMPLYING_DEAD ]
+[ "$(wget -qO - http://localhost:8732/network/connections | jq '. | length')" -gt 1 ]
