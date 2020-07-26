d4a5db7b3974d64a3a5df4516f7853e5e86472a5
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-07-26 11:55:40 +0200

    fixes to make public, private nodes and loadbalancer connect to each other

--- docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
+++ docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
@@ -5,17 +5,5 @@ set -x
 envsubst < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 echo "haproxy tezos signer load balancer is starting"
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- haproxy "$@"
-fi
-
-if [ "$1" = 'haproxy' ]; then
-	shift # "haproxy"
-	# if the user wants "haproxy", let's add a couple useful flags
-	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
-	#   -db -- disables background mode
-	set -- haproxy -W -db "$@"
-fi
-
+set -- haproxy -W -db -f /usr/local/etc/haproxy/haproxy.cfg "$@"
 exec "$@"
