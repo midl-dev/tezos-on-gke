b01a311bcca897fac0aed7a105b6b37584843d45
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-08-03 23:43:32 +0300

    typo

--- docker/tezos-baker-with-remote-signer/tezos-baker-with-remote-signer.sh
+++ docker/tezos-baker-with-remote-signer/tezos-baker-with-remote-signer.sh
@@ -25,14 +25,7 @@ node_data_dir="$node_dir/data"
 
 . "$BIN_DIR/entrypoint.inc.sh"
 
-if [ "$REMOTE_SIGNER_IN_USE" == "true" ]; then
-    remote_signer_param="-R http://tezos-remote-signer-forwarder:8445/$PUBLIC_BAKING_KEY"
-else
-    remote_signer_param=""
-fi
-
 exec "$baker" --chain main \
      --base-dir "$client_dir" \
      --addr "$NODE_HOST" --port "$NODE_RPC_PORT" \
-     $remote_signer_param \
      run with local node "$node_data_dir" "$@"
--- docker/tezos-endorser-with-remote-signer/tezos-endorser-with-remote-signer.sh
+++ docker/tezos-endorser-with-remote-signer/tezos-endorser-with-remote-signer.sh
@@ -25,15 +25,8 @@ node_data_dir="$node_dir/data"
 
 . "$BIN_DIR/entrypoint.inc.sh"
 
-if [ "$REMOTE_SIGNER_IN_USE" == "true" ]; then
-    remote_signer_param="-R http://tezos-remote-signer-forwarder:8445/$PUBLIC_BAKING_KEY"
-else
-    remote_signer_param=""
-fi
-
 wait_for_the_node_to_be_bootstraped
 exec "$endorser" --chain main \
      --base-dir "$client_dir" \
      --addr "$NODE_HOST" --port "$NODE_RPC_PORT" \
-     $remote_signer_param \
      run "$@"
