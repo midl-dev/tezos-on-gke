d98eecd0b5823cc37181c86f1a6f55dfb46cd20d
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-08-04 09:23:46 +0300

    load balancer supports one or two keys
    fix the liveness check for remote signer

--- docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
+++ docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
@@ -2,6 +2,12 @@
 set -e
 set -x
 
+if [ ! -z $SIGNER_A_KEY ]; then
+    SIGNER_A_LINE="server tezos-signer-1 xta-tezos-remote-signer-forwarder:${SIGNER_A_PORT} check inter 15000"
+fi
+if [ ! -z $SIGNER_A_KEY ]; then
+    SIGNER_B_LINE="server tezos-signer-2 xta-tezos-remote-signer-forwarder:${SIGNER_B_PORT} check inter 15000"
+fi
 envsubst < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 echo "haproxy tezos signer load balancer is starting"
 
