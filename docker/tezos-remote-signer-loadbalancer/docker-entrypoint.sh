fe53659a802686b576802773c9e6c420b5e34469
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-08-04 17:32:28 +0300

    fix typos

--- docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
+++ docker/tezos-remote-signer-loadbalancer/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 set -x
 
-if [ ! -z $SIGNER_A_KEY ]; then
+if [ ! -z $SIGNER_A_PORT ]; then
     SIGNER_A_LINE="server tezos-signer-1 xta-tezos-remote-signer-forwarder:${SIGNER_A_PORT} check inter 15000"
 fi
-if [ ! -z $SIGNER_A_KEY ]; then
+if [ ! -z $SIGNER_B_PORT ]; then
     SIGNER_B_LINE="server tezos-signer-2 xta-tezos-remote-signer-forwarder:${SIGNER_B_PORT} check inter 15000"
 fi
 envsubst < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
