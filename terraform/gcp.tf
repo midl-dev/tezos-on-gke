0569225dc7a88849ea68f2919e96fe7f77e69060
Nicolas Ochem <nicolas.ochem@gmail.com>
2020-10-28 16:17:27 -0700

    update to vpc-native and latest version of self-contained cluster

--- terraform/gcp.tf
+++ terraform/gcp.tf
@@ -1,11 +1,12 @@
 module "terraform-gke-blockchain" {
-  source = "github.com/midl-dev/terraform-gke-blockchain?ref=v1.1"
+  source = "github.com/midl-dev/terraform-gke-blockchain?ref=v1.2"
   org_id = var.org_id
   billing_account = var.billing_account
   terraform_service_account_credentials = var.terraform_service_account_credentials
   project = var.project
   project_prefix = "tezos"
   region = var.region
+  vpc_native = false
   node_locations = var.node_locations
   monitoring_slack_url = var.monitoring_slack_url
 }
