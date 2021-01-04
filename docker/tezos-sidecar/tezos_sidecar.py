efd8b111372c618aba5d44a5e7b0257cb279f62a
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-01-04 14:27:21 -0800

    remove startupProbe since GKE does not support it yet
    instead, make liveness probe more tolerant
    
    make sidecar output readable

--- docker/tezos-sidecar/tezos_sidecar.py
+++ docker/tezos-sidecar/tezos_sidecar.py
@@ -7,18 +7,32 @@ application = Flask(__name__)
 
 @application.route('/has_peers')
 def peer_checker():
-    r = requests.get('http://localhost:8732/network/peers')
+    try:
+        r = requests.get('http://localhost:8732/network/peers')
+    except requests.exceptions.RequestException as e:
+        err = "Could not connect to node, %s" % repr(e), 500
+        print(err)
+        return err
     number_of_peers = len(r.json())
     if number_of_peers < 1:
-        raise Exception("We don't have any peer")
+        err = "We don't have any peer", 500
+        print(err)
+        return err
     return str(number_of_peers)
 
 @application.route('/is_synced')
 def sync_checker():
-    r = requests.get('http://localhost:8732/chains/main/blocks/head')
+    try:
+        r = requests.get('http://localhost:8732/chains/main/blocks/head')
+    except requests.exceptions.RequestException as e:
+        err = "Could not connect to node, %s" % repr(e), 500
+        print(err)
+        return err
     most_recent_block_date = parser.isoparse(r.json()["header"]["timestamp"])
     now = datetime.datetime.now(datetime.timezone.utc)
     last_block_age_in_seconds = (now - most_recent_block_date).total_seconds()
     if last_block_age_in_seconds > 240:
-        raise Exception("Last block is %s seconds old, which is over the limit of 240" % last_block_age_in_seconds)
+        err = "Last block is %s seconds old, which is over the limit of 240" % last_block_age_in_seconds, 500
+        print(err)
+        return err
     return str(last_block_age_in_seconds)
