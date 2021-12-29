edbe0ab40b8c4d6374a6f94378bba058520b253a
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-12-29 15:19:44 -0800

    do not login to kubectl, recursive upload

--- docker/payout-report-uploader/entrypoint.sh
+++ docker/payout-report-uploader/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -x
-# workload identity allows this to work
-gcloud container clusters get-credentials blockchain --region $GCP_REGION
 
-cd /app/reports
-
-find
+find /app/reports
 
 echo "now rsyncing payout reports to $REPORT_BUCKET_URL"
-gsutil -m rsync /app/reports $REPORT_BUCKET_URL
+# workload identity allows this to work
+gsutil -m rsync -r /app/reports $REPORT_BUCKET_URL
+
+echo "Done"
+echo ""
